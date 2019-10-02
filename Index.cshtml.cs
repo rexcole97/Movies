ce010280b3e85f23dee7aeda5a0e79c61ed7d99f
rexcole97 <54370507+rexcole97@users.noreply.github.com>
2019-10-02 10:12:55 -0500

    Add files via upload

---
+++ Index.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NoviMovie.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string[] Currentexperiences { get; set; }
+        public void OnGet()
+        {
+            Currentexperiences = new string[] { "Avatar", "We Bought a zoo", "Jurassic Park 3", "National Treasure" };
+        }
+    }
+}
